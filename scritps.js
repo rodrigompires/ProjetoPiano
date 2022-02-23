b17463a324ff8e66c716267fc97098efbaef0aa5
rodrigompires <digo0203@gmail.com>
2022-02-23 19:22:04 -0300

    first commit

---
+++ scritps.js
@@ -0,0 +1,74 @@
+(function () {
+
+    // ADICIONANDO EVENTOS DE CLICK E ALTERANDO SRC DOS AUDIOS
+
+    const $TECLAS = document.querySelectorAll(".tc");
+    let somNota = document.getElementById("sons");
+    let input = document.getElementById("selecao");
+
+    $TECLAS.forEach((t) => {
+
+        t.addEventListener("click", () => {
+
+            let ini = null;
+            let i = input.selectedIndex;
+
+            switch (i) {
+                case 0:
+                    ini = "sons/Normal/";
+                    break;
+                case 1:
+                    ini = "sons/Orgão/";
+                    break;
+                case 2:
+                    ini = "sons/Teclado/";
+                    break;
+                case 3:
+                    ini = "sons/Xilofone/";
+                    break;
+                case 4:
+                    ini = "sons/Teclado Suave/";
+                
+            }
+
+            somNota.src =  `${ini}${t.id}.mp3`
+            somNota.currentTime = 0;
+            somNota.play();
+        });
+    });
+
+
+
+
+    // MAPEAMENTO DAS TECLAS
+
+    const mapaTeclas = {
+        "q" : "C",
+        "w" : "Csus",
+        "e" : "D",
+        "r" : "Dsus",
+        "t" : "E",
+        "y" : "F",
+        "u" : "Fsus",
+        "i" : "G",
+        "o" : "Gsus",
+        "p" : "A",
+        "a" : "Asus",
+        "s" : "B",
+        "d" : "C2",
+        "f" : "C2sus",
+        "g" : "D2",
+        "h" : "D2sus",
+        "j" : "E2"
+    }
+
+    const teclasDigitadas = (evento) => {
+        const tecla = evento.key;
+        const teclaMapeada = () => Object.keys(mapaTeclas).indexOf(tecla) !== -1;
+
+        if (teclaMapeada()) document.getElementById(mapaTeclas[tecla]).click();
+    }
+
+    document.addEventListener("keydown", teclasDigitadas);
+
+})()
